41ec4f07e1909cc761556ff9958a6fd066cffb73
Rishikesh P Vijay <rishikeshpvijay@gmail.com>
2023-06-20 23:06:57 +0530

    fix: firebase token registration issue

--- lib/views/login/login_view_model.dart
+++ lib/views/login/login_view_model.dart
@@ -1,7 +1,11 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:smart_school_bus/app/app.router.dart';
 import 'package:smart_school_bus/app/locator.dart';
 import 'package:smart_school_bus/enums/snackbar_type.dart';
+import 'package:smart_school_bus/models/firestore_collections.dart';
 import 'package:smart_school_bus/services/authentication_service.dart';
 import 'package:smart_school_bus/utils/is_email_valid.dart';
 import 'package:stacked/stacked.dart';
@@ -28,6 +32,14 @@ class LoginViewModel extends BaseViewModel {
     await _navigationService.replaceWith(routeName);
   }
 
+  void handleFCMToken(String fcmToken, User? user) {
+    if (user == null) return;
+    FirebaseFirestore.instance
+        .collection(FireStoreCollections.users)
+        .doc(user.uid)
+        .update({'token': fcmToken});
+  }
+
   void login() async {
     try {
       if (!loginFormKey.currentState!.validate()) return;
@@ -35,6 +47,17 @@ class LoginViewModel extends BaseViewModel {
       notifyListeners();
       loginFormKey.currentState!.save();
       await _authenticationService.login(email.trim(), password.trim());
+
+      final fcm = FirebaseMessaging.instance;
+      final User? user = _authenticationService.getUser();
+      final fcmToken = await fcm.getToken();
+      if (fcmToken != null) handleFCMToken(fcmToken, user);
+      fcm.onTokenRefresh.listen((fcmToken) {
+        handleFCMToken(fcmToken, user);
+      }).onError((err) {
+        // Error getting token.
+      });
+
       _navigationService.clearStackAndShow(Routes.homeView);
     } catch (err) {
       isLogginIn = false;
